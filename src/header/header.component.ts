11aa60e6bb8040406500df6296645cd7d16a2bf6
shsalia <shsalia@napr.gov.ge>
2025-08-18 11:39:52 +0400

    feat: implement header component with template and styles

---
+++ src/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    standalone: true,
+    templateUrl: './header.component.html'
+    
+
+})
+export class HeaderComponent {}
