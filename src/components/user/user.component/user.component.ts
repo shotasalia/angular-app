dc963e5d8f8423d3adf6df6d0d1f82fd624c970e
s.salia <s.salia@gtu.ge>
2025-08-23 18:09:02 +0400

    feat: add my first signal

--- src/components/user/user.component/user.component.ts
+++ src/components/user/user.component/user.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../user.component/data/users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -9,14 +10,14 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return 'users/' + this.selectedUser.avatar;
-  }
+  public selectedUser = signal(DUMMY_USERS[randomIndex]);
+  public imagePath = computed(() => '/users/' + this.selectedUser().avatar);
+  // get imagePath() {
+  //   return 'users/' + this.selectedUser.avatar;
+  // }
 
   onClickButton() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];;
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
