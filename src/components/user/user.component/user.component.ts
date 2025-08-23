b9f2031353b91b8f59884bb8433b8fe12c1f5354
s.salia <s.salia@gtu.ge>
2025-08-23 16:39:06 +0400

    feat: onclick user btn change another user

--- src/components/user/user.component/user.component.ts
+++ src/components/user/user.component/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from '../user.component/data/users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 @Component({
   selector: 'app-user',
   imports: [],
@@ -7,13 +9,14 @@ import { DUMMY_USERS } from '../user.component/data/users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  public selectedUser = DUMMY_USERS[3];
+  public selectedUser = DUMMY_USERS[randomIndex];
 
   get imagePath() {
     return 'users/' + this.selectedUser.avatar;
   }
 
   onClickButton() {
-    console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser = DUMMY_USERS[randomIndex];;
   }
 }
