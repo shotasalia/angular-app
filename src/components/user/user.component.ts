895015ade24306a54b54531770052a384f0fe405
s.salia <s.salia@gtu.ge>
2025-08-24 20:22:40 +0400

    feat: add for loop in app.html && change @Input decorator name, avatar, id on object && create user interface

--- src/components/user/user.component.ts
+++ src/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, EventEmitter, input, Input, Output, signal } from '@angular/core';
 import { DUMMY_USERS } from './data/users';
+import { User } from './interface/user.interface';
 
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,18 +12,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user !: User
   @Output() select = new EventEmitter<string>();
   
   
   get imagePath() {
-    return '/users/' + this.avatar;
+    return '/users/' + this.user.avatar;
   }
 
 
   onSelectUser () {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
